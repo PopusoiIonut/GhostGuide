9d1692bbf6c3bb18eb691188c49a70832dbee7c6
Ionut Popusoi <popusoiionut@yahoo.com>
2026-04-28 10:07:11 +0100

    Initial Commit

---
+++ Ghost GuideTests/Ghost_GuideTests.swift
@@ -0,0 +1,19 @@
+//
+//  Ghost_GuideTests.swift
+//  Ghost GuideTests
+//
+//  Created by USER on 28/04/2026.
+//
+
+import Testing
+@testable import Ghost_Guide
+
+struct Ghost_GuideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
